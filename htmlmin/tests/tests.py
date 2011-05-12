dd10d42aa091003530c3a5b632fd3723bf6d4bdb
andrews medina <andrewsmedina@gmail.com>
2011-05-12 13:47:19 -0300

    added funciton to minify a simple html

---
+++ htmlmin/tests/tests.py
@@ -0,0 +1,16 @@
+import unittest
+
+from htmlmin.minify import html_minify
+
+
+class MinifyTestCase(unittest.TestCase):
+
+    def test_html_should_be_minified(self):
+        html = "<html>   <body>some text here</body>    </html>"
+
+        html_minified = "<html><body>some text here</body></html>"
+
+        self.assertEqual(html_minified, html_minify(html))
+
+
+unittest.main()
