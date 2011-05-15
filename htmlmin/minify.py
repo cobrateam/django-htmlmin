cd2f62be6a77ec4910a775d753b6443b97a94f03
Francisco Souza <francisco@franciscosouza.net>
2011-05-15 20:25:51 -0300

    Very ugly test for excluding <script> from minifying

--- htmlmin/minify.py
+++ htmlmin/minify.py
@@ -5,10 +5,22 @@ def html_minify(html_code):
     dom = html.fromstring(html_code)
     html_code = html.tostring(dom, method='xml', encoding=unicode)
 
+    script = False
     minified_lines = []
     for line in html_code.split('\n'):
-        minified_line = line.strip()
-        if minified_line:
-            minified_lines.append(minified_line)
+        if '</script>' in line:
+            script = False
+
+        if script:
+            minified_line = "%s\n" % line.rstrip()
+        else:
+            minified_line = line.strip()
+
+        minified_lines.append(minified_line)
+
+        if '<script' in line:
+            script = True
+            first = True
+            minified_lines.append('\n')
 
     return u"".join(minified_lines)
