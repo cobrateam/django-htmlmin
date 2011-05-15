804f603a28cb158f93c91bc5e33e37882eb9865a
andrews medina <andrewsmedina@gmail.com>
2011-05-15 17:15:08 -0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+README = open('README.rst').read()
+
+setup(name='django-htmlmin',
+      version='0.0.1',
+      description='html minify for django',
+      long_description=README,
+      author='CobraTeam',
+      author_email='andrewsmedina@gmail.com',
+      packages=find_packages(),
+      include_package_data=True,
+      test_suite='nose.collector',
+      install_requires=['django', 'lxml'],
+      tests_require=['nose', 'coverage'],
+      )
