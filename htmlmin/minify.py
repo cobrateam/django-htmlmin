004e4db93328ea742340b9255b6be1dd436960f0
Francisco Souza <francisco@franciscosouza.net>
2011-05-17 03:15:57 -0300

    Using a regex for comment ignoring (definitely closes #2)

--- htmlmin/minify.py
+++ htmlmin/minify.py
@@ -1,21 +1,6 @@
+import re
 from lxml import html
 from util import force_decode
-
-def filter_lines(lines):
-    code_lines = []
-    is_comment = False
-    for line in lines:
-        stripped_line = line.strip()
-        if stripped_line.startswith('<!--'):
-            is_comment = True
-
-        if not is_comment:
-            code_lines.append(line)
-
-        if stripped_line.endswith('-->'):
-            is_comment = False
-
-    return code_lines
 
 def html_minify(html_code, ignore_comments=True):
     html_code = force_decode(html_code)
@@ -24,11 +9,8 @@ def html_minify(html_code, ignore_comments=True):
 
     script = False
     lines = html_code.split('\n')
-
-    if ignore_comments:
-        lines = filter_lines(lines)
-
     minified_lines = []
+
     for line in lines:
         if '</script>' in line:
             script = False
@@ -48,5 +30,9 @@ def html_minify(html_code, ignore_comments=True):
 
     if "DOCTYPE" not in content:
         content = "<!DOCTYPE html>%s" % content
+
+    if ignore_comments:
+        comment_regexes = re.compile('<!--.*-->')
+        content = comment_regexes.sub('', content)
 
     return content
