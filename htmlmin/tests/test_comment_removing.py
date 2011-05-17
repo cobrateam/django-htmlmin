27cbae8427c5fb38428e15a476f86bc2e2de0169
Francisco Souza <francisco@franciscosouza.net>
2011-05-17 10:28:55 -0300

    Extrating tests for comments

---
+++ htmlmin/tests/test_comment_removing.py
@@ -0,0 +1,9 @@
+import unittest
+from htmlmin.minify import drop_comments
+
+class TestCommentRemoving(unittest.TestCase):
+
+    def test_nested_comments_should_be_also_excluded(self):
+        html = '<html><body><!-- comment with <!-- comment inside --></body></html>'
+        expected = '<html><body></body></html>'
+        self.assertEqual(expected, drop_comments(html))
