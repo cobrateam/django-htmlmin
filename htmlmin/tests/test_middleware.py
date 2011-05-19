3551e52cd88bb4b59f50d287f2f40aa2c5b6abe3
Francisco Souza <francisco@franciscosouza.net>
2011-05-19 10:53:00 -0300

    Fixing test name

--- htmlmin/tests/test_middleware.py
+++ htmlmin/tests/test_middleware.py
@@ -27,7 +27,7 @@ class TestMiddleware(unittest.TestCase):
         html_minified = "<!DOCTYPE html><html><body>some text here</body></html>"
         self.assertEqual(html_minified, response.content)
 
-    def test_middleware_should_not_be_minify_response_when_mime_type_not_is_html(self):
+    def test_middleware_should_not_minify_response_when_mime_type_not_is_html(self):
         response_mock = ResponseMock()
         response_mock['Content-Type'] = 'application/json'
         response = HtmlMinifyMiddleware().process_response(None, response_mock)
