057e92d37c705787e10454333a23141f00490933
Francisco Souza <francisco@franciscosouza.net>
2011-05-19 11:52:28 -0300

    Added a setting EXCLUDE_FROM_MINIFYING for not minify some URL patterns

--- htmlmin/middleware.py
+++ htmlmin/middleware.py
@@ -1,8 +1,21 @@
+import re
 from htmlmin.minify import html_minify
+from django.conf import settings
 
 class HtmlMinifyMiddleware(object):
 
+    def can_minify_response(self, request, response):
+        is_request_ok = True
+        for url_pattern in settings.EXCLUDE_FROM_MINIFYING:
+            regex = re.compile(url_pattern)
+            if regex.match(request.path.lstrip('/')):
+                is_request_ok = False
+                break
+
+        is_response_ok = response.status_code == 200 and 'text/html' in response['Content-Type']
+        return is_request_ok and is_response_ok
+
     def process_response(self, request, response):
-        if response.status_code == 200 and 'text/html' in response['Content-Type']:
+        if self.can_minify_response(request, response):
             response.content = html_minify(response.content)
         return response
--- htmlmin/tests/test_middleware.py
+++ htmlmin/tests/test_middleware.py
@@ -1,20 +1,33 @@
+import os
+import sys
 import unittest
 from htmlmin.middleware import HtmlMinifyMiddleware
-from mocks import ResponseMock
+from htmlmin.tests import TESTS_DIR
+from mocks import RequestMock, ResponseMock
 
 class TestMiddleware(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        sys.path.insert(0, TESTS_DIR)
+        os.environ['DJANGO_SETTINGS_MODULE'] = 'mock_settings'
+
+    @classmethod
+    def tearDownClass(cls):
+        sys.path.remove(TESTS_DIR)
+        del os.environ['DJANGO_SETTINGS_MODULE']
 
     def test_middleware_should_minify_only_when_status_code_is_200(self):
         response_mock = ResponseMock()
         response_mock.status_code = 301
-        response = HtmlMinifyMiddleware().process_response(None, response_mock)
+        response = HtmlMinifyMiddleware().process_response(RequestMock(), response_mock)
 
         html_not_minified = "<html>   <body>some text here</body>    </html>"
         self.assertEqual(html_not_minified, response.content)
 
     def test_middleware_should_be_minify_response_when_mime_type_is_html(self):
         response_mock = ResponseMock()
-        response = HtmlMinifyMiddleware().process_response(None, response_mock)
+        response = HtmlMinifyMiddleware().process_response(RequestMock(), response_mock)
 
         html_minified = "<!DOCTYPE html><html><body>some text here</body></html>"
         self.assertEqual(html_minified, response.content)
@@ -22,7 +35,7 @@ class TestMiddleware(unittest.TestCase):
     def test_middleware_should_minify_with_any_charset(self):
         response_mock = ResponseMock()
         response_mock['Content-Type'] = 'text/html; charset=utf-8'
-        response = HtmlMinifyMiddleware().process_response(None, response_mock)
+        response = HtmlMinifyMiddleware().process_response(RequestMock(), response_mock)
 
         html_minified = "<!DOCTYPE html><html><body>some text here</body></html>"
         self.assertEqual(html_minified, response.content)
@@ -30,7 +43,15 @@ class TestMiddleware(unittest.TestCase):
     def test_middleware_should_not_minify_response_when_mime_type_not_is_html(self):
         response_mock = ResponseMock()
         response_mock['Content-Type'] = 'application/json'
-        response = HtmlMinifyMiddleware().process_response(None, response_mock)
+        response = HtmlMinifyMiddleware().process_response(RequestMock(), response_mock)
 
         html_not_minified = "<html>   <body>some text here</body>    </html>"
         self.assertEqual(html_not_minified, response.content)
+
+    def test_middleware_should_not_minify_the_response_on_urls_that_are_configured_to_not_be_minified(self):
+        html_not_minified = "<html>   <body>some text here</body>    </html>"
+        response_mock = ResponseMock()
+        response = HtmlMinifyMiddleware().process_response(RequestMock('/raw/'), response_mock)
+        self.assertEqual(html_not_minified, response.content)
+
+        self.mocker.verify()
