8c98e830af9137a1ad33ffd48fb2754a9a419092
Francisco Souza <francisco@franciscosouza.net>
2011-05-21 16:56:32 -0300

    refactoring the non-script minifying

--- htmlmin/minify.py
+++ htmlmin/minify.py
@@ -1,43 +1,33 @@
 from BeautifulSoup import BeautifulSoup, Comment
 from util import force_decode
 
+SCRIPT_PATTERN = '############ script %d ############'
+
 def html_minify(html_code, ignore_comments=True):
     html_code = force_decode(html_code)
-
     soup = BeautifulSoup(html_code)
 
     if ignore_comments:
-        comments = soup.findAll(text=lambda text:isinstance(text, Comment))
-        [comment.extract() for comment in comments]
+        [ comment.extract() for comment in soup.findAll(text=lambda text:isinstance(text, Comment)) ]
 
+    scripts = [ str(script) for script in soup.findAll(name='script') if len(script.text) > 0 ]
+
+    for index, script in enumerate(scripts):
+        html_code = html_code.replace(script, SCRIPT_PATTERN % index)
+
+    soup = BeautifulSoup(html_code)
     html_code = soup.prettify()
-    script = False
     lines = html_code.split('\n')
     minified_lines = []
 
-    last_line = '>'
     for line in lines:
-        if '</script>' in line:
-            script = False
-
-        if script:
-            minified_line = u"%s\n" % line.rstrip()
-        else:
-            minified_line = line.strip()
-
-        if (minified_line.startswith('<') and not minified_line.startswith('</') and not last_line.endswith('>')):
-            minified_lines.append(' ')
-
+        minified_line = line.strip()
         minified_lines.append(str(minified_line))
 
-        if '<script' in line:
-            script = True
-            minified_lines.append('\n')
-
-        last_line = minified_line
-
-
     content = "".join(minified_lines)
+
+    for index, script in enumerate(scripts):
+        content = content.replace(SCRIPT_PATTERN % index, script)
 
     if "DOCTYPE" not in content:
         content = "<!DOCTYPE html>%s" % content
