a2bd8140fc72cb11003c23be36398b42ad39b741
Francisco Souza <francisco@franciscosouza.net>
2011-05-21 01:39:18 -0300

    Removed custom Cleaner

--- htmlmin/minify.py
+++ htmlmin/minify.py
@@ -1,23 +1,10 @@
-import re
-from lxml.html.clean import Cleaner as HTMLCleaner
+from lxml.html.clean import Cleaner
 from util import force_decode
-
-class Cleaner(object):
-
-    def __init__(self):
-        self.spaces_regex = re.compile('\s+')
-        self.comment_regexes = re.compile('<!--(.|\s)*?-->')
-
-    def drop_comments(self, content):
-        return self.comment_regexes.sub('', content)
-
-    def remove_spaces(self, html_code):
-        return self.spaces_regex.sub('', html_code)
 
 def html_minify(html_code, ignore_comments=True):
     html_code = force_decode(html_code)
 
-    cleaner = HTMLCleaner(scripts=False, javascript=False, comments=ignore_comments, links=False, meta=False, page_structure=False, processing_instructions=False, embedded=False, frames=False, forms=False, annoying_tags=False, remove_unknown_tags=False, safe_attrs_only=False)
+    cleaner = Cleaner(scripts=False, javascript=False, comments=ignore_comments, links=False, meta=False, page_structure=False, processing_instructions=False, embedded=False, frames=False, forms=False, annoying_tags=False, remove_unknown_tags=False, safe_attrs_only=False)
     html_code = cleaner.clean_html(html_code)
 
     script = False
