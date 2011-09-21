14c30560edd69cb6ec37ab48abce6945279a2aaf
Francisco Souza <f@souza.cc>
2011-09-21 01:02:48 -0300

    Bad whitespace

--- htmlmin/minify.py
+++ htmlmin/minify.py
@@ -9,10 +9,10 @@ def html_minify(html_code, ignore_comments=True):
     html_code = force_decode(html_code)
     soup = BeautifulSoup(html_code)
     exclude_tags = {}
-    
+
     for tag in EXCLUDE_TAGS:
         exclude_tags[tag] = [ str(script) for script in soup.findAll(name=tag) if len(script.text) > 0 ]
-        
+
         for index, script in enumerate(exclude_tags[tag]):
             html_code = html_code.replace(script, TAGS_PATTERN % (tag, index))
 
@@ -31,7 +31,7 @@ def html_minify(html_code, ignore_comments=True):
         minified_lines.append(str(minified_line))
 
     content = "".join(minified_lines)
-    
+
     for tag in EXCLUDE_TAGS:
         for index, script in enumerate(exclude_tags[tag]):
             content = content.replace(TAGS_PATTERN % (tag,index), script)
