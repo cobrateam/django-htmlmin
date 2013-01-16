7fef3061555d2c1a1ffb5a9f68af328570dc75bf
Francisco Souza <f@souza.cc>
2013-01-16 21:33:19 -0200

    htmlmin/tests: don't use assert_equals

--- htmlmin/tests/test_minify.py
+++ htmlmin/tests/test_minify.py
@@ -4,10 +4,10 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+import codecs
 import unittest
 
 from htmlmin.minify import html_minify
-from nose.tools import assert_equals
 from os.path import abspath, dirname, join
 
 resources_path = lambda *paths: abspath(join(dirname(__file__), 'resources', *paths))
@@ -20,87 +20,87 @@ class TestMinify(unittest.TestCase):
         html_file_minified = resources_path('%s_minified.html' % filename)
 
         html = open(html_file).read()
-        html_minified = open(html_file_minified).read().strip('\n')
+        html_minified = codecs.open(html_file_minified, encoding='utf-8').read().strip('\n')
 
         return html, html_minified
 
     def test_complete_html_should_be_minified(self):
         html, html_minified = self._get_normal_and_minified_content_from_html_files('with_menu')
-        assert_equals(html_minified, html_minify(html))
+        self.assertEqual(html_minified, html_minify(html))
 
     def test_html_with_blank_lines_should_be_minify(self):
         html, html_minified = self._get_normal_and_minified_content_from_html_files('with_blank_lines')
-        assert_equals(html_minified, html_minify(html))
+        self.assertEqual(html_minified, html_minify(html))
 
     def test_should_not_minify_content_from_script_tag(self):
         html, html_minified = self._get_normal_and_minified_content_from_html_files('with_javascript')
-        assert_equals(html_minified, html_minify(html))
+        self.assertEqual(html_minified, html_minify(html))
 
     def test_should_not_minify_content_from_pre_tag(self):
         html, html_minified = self._get_normal_and_minified_content_from_html_files('with_pre')
-        assert_equals(html_minified, html_minify(html))
+        self.assertEqual(html_minified, html_minify(html))
 
     def test_should_not_minify_content_from_textarea(self):
         html, html_minified = self._get_normal_and_minified_content_from_html_files('with_textarea')
         result = html_minify(html)
-        assert_equals(html_minified, result)
+        self.assertEqual(html_minified, result)
 
     def test_should_not_drop_blank_lines_from_the_begin_of_a_textarea(self):
         html, html_minified = self._get_normal_and_minified_content_from_html_files('with_textarea_with_blank_lines')
         result = html_minify(html)
-        assert_equals(html_minified, result)
+        self.assertEqual(html_minified, result)
 
     def test_html_should_be_minified(self):
         html = "<html>   <body>some text here</body>    </html>"
         html_minified = "<html><head></head><body>some text here </body></html>"
-        assert_equals(html_minified, html_minify(html))
+        self.assertEqual(html_minified, html_minify(html))
 
     def test_minify_function_should_return_a_unicode_object(self):
         html = "<html>   <body>some text here</body>    </html>"
         html_minified = html_minify(html)
-        assert_equals(unicode, type(html_minified))
+        self.assertEqual(unicode, type(html_minified))
 
     def test_minify_should_respect_encoding(self):
         html, html_minified = self._get_normal_and_minified_content_from_html_files('blogpost')
-        assert_equals(html_minified, html_minify(html))
+        self.assertEqual(html_minified, html_minify(html))
 
     def test_minify_should_not_prepend_doctype_when_its_not_present(self):
         html, html_minified = self._get_normal_and_minified_content_from_html_files('without_doctype')
-        assert_equals(html_minified, html_minify(html))
+        self.assertEqual(html_minified, html_minify(html))
 
     def test_minify_should_keep_doctype_when_its_present(self):
         html, html_minified = self._get_normal_and_minified_content_from_html_files('with_old_doctype')
-        assert_equals(html_minified, html_minify(html))
+        self.assertEqual(html_minified, html_minify(html))
 
     def test_should_exclude_comments_by_default(self):
         html, html_minified = self._get_normal_and_minified_content_from_html_files('with_comments_to_exclude')
-        assert_equals(html_minified, html_minify(html))
+        self.assertEqual(html_minified, html_minify(html))
 
     def test_should_be_able_to_not_exclude_comments(self):
         html, html_minified = self._get_normal_and_minified_content_from_html_files('with_comments')
-        assert_equals(html_minified, html_minify(html, ignore_comments=False))
+        self.assertEqual(html_minified, html_minify(html, ignore_comments=False))
 
     def test_should_be_able_to_exclude_multiline_comments(self):
         html, html_minified = self._get_normal_and_minified_content_from_html_files('with_multiple_line_comments')
-        assert_equals(html_minified, html_minify(html))
+        self.assertEqual(html_minified, html_minify(html))
 
     def test_should_be_able_to_exclude_multiple_comments_on_a_page(self):
         html, html_minified = self._get_normal_and_minified_content_from_html_files('with_multiple_comments')
-        assert_equals(html_minified, html_minify(html))
+        self.assertEqual(html_minified, html_minify(html))
 
     def test_should_keep_html_attributes_intact(self):
         html = '<html><body>\n<select name="gender" multiple="multiple">\n    <option value="M" selected="selected">Male</option>\n    <option value="F">Female</option>\n</select></body></html>'
         html_minified = '<html><head></head><body><select multiple="multiple" name="gender"><option selected="selected" value="M">Male</option><option value="F">Female</option></select></body></html>'
-        assert_equals(html_minified, html_minify(html))
+        self.assertEqual(html_minified, html_minify(html))
 
     def test_should_touch_attributes_only_on_tags(self):
         html = '<html>\n    <body>I selected you!</body>\n    </html>'
         html_minified = '<html><head></head><body>I selected you!</body></html>'
-        assert_equals(html_minified, html_minify(html))
+        self.assertEqual(html_minified, html_minify(html))
 
     def test_should_be_able_to_minify_html5_tags(self):
         html, html_minified = self._get_normal_and_minified_content_from_html_files('html5')
-        assert_equals(html_minified, html_minify(html))
+        self.assertEqual(html_minified, html_minify(html))
 
     def test_should_transform_a_lot_of_extra_white_spaces_in_a_unique_one(self):
         html = '<html><body><a href="foo-bar">google!</a>     <a href="http://cobrateam.info">cobrateam</a></body></html>'
