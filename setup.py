7d606a237290fa27e20c33ed79d37ba110350295
Francisco Souza <f@souza.cc>
2013-01-16 23:05:13 -0200

    setup: flake8 fixes

--- setup.py
+++ setup.py
@@ -9,20 +9,21 @@ from htmlmin import __version__
 
 README = open('README.rst').read()
 
-setup(name='django-htmlmin',
-      version=__version__,
-      description='html minify for django',
-      long_description=README,
-      author='CobraTeam',
-      author_email='andrewsmedina@gmail.com',
-      packages=find_packages(),
-      include_package_data=True,
-      test_suite='nose.collector',
-      install_requires=['argparse', 'django', 'beautifulsoup4', 'html5lib'],
-      tests_require=['nose', 'coverage'],
-      entry_points = {
-          'console_scripts' : [
-              'pyminify = htmlmin.commands:main',
-          ]
-      },
-     )
+setup(
+    name='django-htmlmin',
+    version=__version__,
+    description='html minify for django',
+    long_description=README,
+    author='CobraTeam',
+    author_email='andrewsmedina@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    test_suite='nose.collector',
+    install_requires=['argparse', 'django', 'beautifulsoup4', 'html5lib'],
+    tests_require=['nose', 'coverage'],
+    entry_points={
+        'console_scripts': [
+            'pyminify = htmlmin.commands:main',
+        ],
+    },
+)
