b85c487185dd8e6855ee59f8c5c4077bb5a1118c
Francisco Souza <f@souza.cc>
2013-01-16 23:05:35 -0200

    htmlmin/__init__: bump to 0.6

--- htmlmin/__init__.py
+++ htmlmin/__init__.py
@@ -1,5 +1,5 @@
-# Copyright 2012 django-htmlmin authors. All rights reserved.
+# Copyright 2013 django-htmlmin authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-__version__ = '0.5.2'
+__version__ = '0.6'
