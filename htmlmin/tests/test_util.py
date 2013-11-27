1060152569a22fb3f12e63a2d56d7be278f73560
Matthias Bauer <matthias.bauer@intosite.de>
2013-11-27 12:19:10 +0100

    Add tests for between_two_tags
    util is now at 100% coverage

--- htmlmin/tests/test_util.py
+++ htmlmin/tests/test_util.py
@@ -5,7 +5,7 @@
 # license that can be found in the LICENSE file.
 
 import unittest
-from htmlmin.util import force_decode
+from htmlmin.util import force_decode, between_two_tags
 
 
 class TestUtil(unittest.TestCase):
@@ -24,3 +24,23 @@ class TestUtil(unittest.TestCase):
         unicode_object = "Blá blá".decode("utf-8").encode(ENCODING)
         string = str(unicode_object)
         self.assertEqual(u"Blá blá", force_decode(string, encoding=ENCODING))
+
+    def test_should_be_between_two_tags(self):
+        all_lines = [
+            '<script type="text/javascript">',
+            'alert("Hello World!");',
+            '</script>',
+            '<p>Hello ',
+            'World!</p>'
+            ]
+        self.assertTrue(between_two_tags(all_lines[1], all_lines, 1))
+
+    def test_should_not_be_between_two_tags(self):
+        all_lines = [
+            '<script type="text/javascript">',
+            'alert("Hello World!");',
+            '</script>',
+            '<p>Hello ',
+            'World!</p>'
+            ]
+        self.assertFalse(between_two_tags(all_lines[4], all_lines, 4))
