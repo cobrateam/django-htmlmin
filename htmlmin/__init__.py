3cb2e190bf06a1eb0c9909f6ff58ac0cc7ced0e4
Andrews Medina <andrewsmedina@gmail.com>
2016-01-30 01:30:29 -0200

    bump to 0.9.0

--- htmlmin/__init__.py
+++ htmlmin/__init__.py
@@ -1,5 +1,5 @@
-# Copyright 2014 django-htmlmin authors. All rights reserved.
+# Copyright 2016 django-htmlmin authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
-__version__ = '0.8.0'
+__version__ = '0.9.0'
