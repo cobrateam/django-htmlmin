969fe83e3f6aee682939dd3289d63fb7b7dbc461
john-nguyen09 <john.nguyen09@outlook.com>
2016-08-19 08:13:14 +1000

    Fix indentation

--- htmlmin/middleware.py
+++ htmlmin/middleware.py
@@ -9,59 +9,59 @@ from htmlmin.minify import html_minify
 
 
 class MarkRequestMiddleware(object):
-	def __init__(self, get_response = None):
-		self.get_response = get_response
-	
-	
-	def __call__(self, request):
-		self.process_request(request)
-	
-		response = self.get_response(request)
-		
-		return response
 
-	def process_request(self, request):
-		request._hit_htmlmin = True
+    def __init__(self, get_response = None):
+        self.get_response = get_response
+	
+	
+    def __call__(self, request):
+        self.process_request(request)
+	
+        response = self.get_response(request)
+		
+        return response
+
+    def process_request(self, request):
+        request._hit_htmlmin = True
 
 
 class HtmlMinifyMiddleware(object):
-	def __init__(self, get_response = None):
-		self.get_response = get_response
-		
-		
-	def __call__(self, request):
-		response = self.get_response(request)
-		
-		self.process_response(request, response)
-		
-		return response
-		
-		
-	def can_minify_response(self, request, response):
-		try:
-			req_ok = request._hit_htmlmin
-		except AttributeError:
-			return False
 
-		if hasattr(settings, 'EXCLUDE_FROM_MINIFYING'):
-			for url_pattern in settings.EXCLUDE_FROM_MINIFYING:
-				regex = re.compile(url_pattern)
-				if regex.match(request.path.lstrip('/')):
-					req_ok = False
-					break
+    def __init__(self, get_response = None):
+        self.get_response = get_response
+		
+		
+    def __call__(self, request):
+        response = self.get_response(request)
+		
+        self.process_response(request, response)
+		
+        return response
 
-		resp_ok = 'text/html' in response.get('Content-Type', '')
-		if hasattr(response, 'minify_response'):
-			resp_ok = resp_ok and response.minify_response
-		return req_ok and resp_ok
+    def can_minify_response(self, request, response):
+        try:
+            req_ok = request._hit_htmlmin
+        except AttributeError:
+            return False
 
+        if hasattr(settings, 'EXCLUDE_FROM_MINIFYING'):
+            for url_pattern in settings.EXCLUDE_FROM_MINIFYING:
+                regex = re.compile(url_pattern)
+                if regex.match(request.path.lstrip('/')):
+                    req_ok = False
+                    break
 
-	def process_response(self, request, response):
-		minify = getattr(settings, "HTML_MINIFY", not settings.DEBUG)
-		keep_comments = getattr(settings, 'KEEP_COMMENTS_ON_MINIFYING', False)
-		parser = getattr(settings, 'HTML_MIN_PARSER', 'html5lib')
-		if minify and self.can_minify_response(request, response):
-			response.content = html_minify(response.content,
-											ignore_comments=not keep_comments,
-											parser=parser)
-		return response
+        resp_ok = 'text/html' in response.get('Content-Type', '')
+        if hasattr(response, 'minify_response'):
+            resp_ok = resp_ok and response.minify_response
+        return req_ok and resp_ok
+
+    def process_response(self, request, response):
+        minify = getattr(settings, "HTML_MINIFY", not settings.DEBUG)
+        keep_comments = getattr(settings, 'KEEP_COMMENTS_ON_MINIFYING', False)
+        parser = getattr(settings, 'HTML_MIN_PARSER', 'html5lib')
+        if minify and self.can_minify_response(request, response):
+            response.content = html_minify(response.content,
+                                           ignore_comments=not keep_comments,
+                                           parser=parser)
+        return response
